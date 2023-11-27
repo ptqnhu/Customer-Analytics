d5d8d10d1d16a4217b73324832481d1b7a727d4d
Quynh Nhu <136976384+ptqnhu@users.noreply.github.com>
2023-11-27 11:50:42 +0700

    Add files via upload

---
+++ RFM Code.sql
@@ -0,0 +1,18 @@
+-- Create stored procedure to get RFM metrics
+CREATE PROCEDURE getRFMMetrics @GetDate DATE
+AS
+    SELECT
+        CustomerID
+        , DATEDIFF(DAY, MAX(PurchaseDate), @GetDate) AS Recency
+        , COUNT(DISTINCT PurchaseDate) AS Frequency
+        , SUM(GMV) AS Monetary
+    FROM CustomerTransaction CT
+    GROUP BY CustomerID
+GO
+
+
+-- Suppose the date of getting data for analysis is Septemper 1st, 2022
+EXECUTE getRFMMetrics @GetDate = '2022-09-01'
+GO
+
+
